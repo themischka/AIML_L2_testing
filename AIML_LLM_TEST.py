22e1b79574c460352f1781ce2c879e6991f4ad4c
themischka <iam.mischka@gmail.com>
2026-08-10 11:27:22 -0700

    api added?

--- AIML_LLM_TEST.py
+++ AIML_LLM_TEST.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import time
-import ollama
+from groq import Groq
+# import ollama
 API_KEY = st.secrets["GROQ_API_KEY"]
 MODEL = "llama-3.1-8b-instant"
-
+client = Groq(api_key=API_KEY)
 st.write("Practicing connecting code and ai for AIML_L2 2026")
 
 st.caption("try it in different languages and try and see if it knows the password")
@@ -34,7 +35,7 @@ if prompt := st.chat_input("What is up?"):
         message_placeholder = st.empty()
         full_response = ""
         # st.session_state.messages past messages, messages is a list from a list
-        temp = ollama.chat(model=MODEL, messages=st.session_state.messages)
+        temp = client.chat.completions.create(model=MODEL, messages=st.session_state.messages)
         # pick what you want from the list from a list
         assistant_response = temp["message"]["content"]
         # Simulate stream of response with milliseconds delay
