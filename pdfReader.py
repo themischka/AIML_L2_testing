2de2ec6414251f409fccfd56860158b18838ec7f
themischka <iam.mischka@gmail.com>
2026-08-14 11:37:59 -0700

    removed unecessary lines that were already commented out

--- pdfReader.py
+++ pdfReader.py
@@ -96,10 +96,6 @@ with tab1:
             chunks.append(text[i:i + st.session_state.chunk_size])
         st.sidebar.write(f"Chunk size: {st.session_state.chunk_size}")
         st.sidebar.write(f"Overlap: {st.session_state.overlap}")
-        # print(len(chunks), "chunks: ")
-
-        # for x in chunks:
-        #     print(x)
         st.write(len(chunks))
         client = chromadb.Client()
         # something here about check if doc is already in
@@ -109,32 +105,7 @@ with tab1:
         collection.add(documents=chunks, ids=tags)
         st.session_state.collection = collection
         st.write("Chunks added to knowledge base")
-        data = file.read()
-        # with st.container():
-        #     st.subheader("PDF Preview")
-        #
-        #     base64_pdf = base64.b64encode(data).decode("utf-8")
-        #
-        #     pdf_display = f"""
-        #     <iframe
-        #         src="data:application/pdf;base64,{base64_pdf}"
-        #         width="100%"
-        #         height="800"
-        #         type="application/pdf">
-        #     </iframe>
-        #     """
-        #
-        #     st.markdown(pdf_display, unsafe_allow_html=True)
     question = st.text_input("ask about the doc")
-    # if question := st.chat_input("Ask about the document"):
-    #     st.session_state.messages.append({"role": "user", "content": question})
-    #     with st.chat_message("user"):
-    #         st.markdown(question)
-    #
-    #     with st.chat_message("assistant"):
-    #         message_placeholder = st.empty()
-    #         full_response = ""
-    #         st.session_state.response = client.chat.completions.create(model=MODEL, messages=st.session_state.messages)
     st.session_state.messages.append({"role": "assistant", "content": st.session_state.response})
     if st.button("Search"):
         st.write("Thinking!")
@@ -143,22 +114,14 @@ with tab1:
         # distances compared to thresh hold to filter out bad results
         for x in range(1):
             if result["distances"][0][x] < disThresh:
-                # print(result["distances"])
-                # print(result["ids"])
                 st.session_state.tone = "Respond confidently."
                 st.session_state.hallucinating = False
                 st.session_state.lost = False
-                # print(st.session_state.tone)
             elif lostThresh > result["distances"][0][x] > disThresh:
-                # print(result["distances"])
-                # print(result["ids"])
                 st.session_state.hallucinating = True
                 st.session_state.lost = False
                 st.session_state.tone = "Respond doubtfully."
-                # print(st.session_state.tone)
             else:
-                # print(result["distances"])
-                # print(result["ids"])
                 st.session_state.tone = "Be very doubtful, there is likely not enough information to make a response"
                 # print(st.session_state.tone)
                 st.session_state.lost = True
@@ -166,8 +129,6 @@ with tab1:
         # change this so that it prints documents of relevant +- 3 ids
         st.session_state.context = result["documents"][0][::-1]
         st.session_state.question = question
-        # for ans in st.session_state.context:
-        #     st.write(ans)
         context = "\n".join(st.session_state.context)
         question = st.session_state.question
 
@@ -188,22 +149,6 @@ with tab1:
             container.markdown(st.session_state.answer)
             container.write("Don't forget I get confused too!")
 
-    # if st.button("LLM answer"):
-    #     context = "\n".join(st.session_state.context)
-    #     question = st.session_state.question
-    #
-    #     messages = [
-    #         {"role": "system", "content": f"Answer if the user's question using only the provided document context. If the context contains enough information to answer, give the answer. {tone}"},
-    #         {"role": "user", "content": f"DOCUMENT CONTEXT: \n{context}\n\nQUESTION:\n{question}"}
-    #     ]
-    #     response = client.chat.completions.create(model=MODEL, messages=messages)
-    #     st.write("LLM answer: ", response.choices[0].message.content)
-    #     if st.session_state.hallucinating is True:
-    #         st.write("With that being said, I might be confused with something else, make sure to double check!")
-    #     elif st.session_state.lost is True:
-    #         st.write("I am definitely lost, try your question again or check the contents of you pdf to see if it is relevant.")
-    #     else:
-    #         st.write("Don't forget I get confused too!")
 with tab2:
     st.title("Read from sample PDFs")
     st.write("Click on either of the samples, wait for them to load, then ask a question.")
@@ -231,8 +176,8 @@ with tab2:
             tags = [sampleFile + str(i) for i in range(len(chunks))]
             collection.add(documents=chunks, ids=tags)
             st.session_state.collection = collection
-            st.write("Chunks added to knowledge base")
-        st.write("Sample 1 is about Ponyo (2008)")
+            st.write("Chunks added to knowledge base.")
+        st.write("Sample 1 is about Ponyo (2008).")
     with col2:
         if st.button("Sample 2"):
             chunks = []
@@ -256,7 +201,7 @@ with tab2:
             collection.add(documents=chunks, ids=tags)
             st.session_state.collection = collection
             st.write("Chunks added to knowledge base")
-        st.write("Sample 2 is about Spirited Away (2001)")
+        st.write("Sample 2 is about Spirited Away (2001).")
     with col3:
         if st.button("Sample 3"):
             chunks = []
@@ -280,7 +225,7 @@ with tab2:
             collection.add(documents=chunks, ids=tags)
             st.session_state.collection = collection
             st.write("Chunks added to knowledge base")
-        st.write("Sample 3 is about what are RAGs")
+        st.write("Sample 3 is about what are RAGs and this website.")
 
     question = st.text_input("Ask a question about what the file is about.", placeholder="Enter here")
     if st.button("search about the sample"):
@@ -302,17 +247,6 @@ with tab2:
         response = client.chat.completions.create(model=MODEL, messages=messages)
         st.session_state.answer = response.choices[0].message.content
         container.markdown(st.session_state.answer)
-    # if st.button("LLM sample pdf answer"):
-    #     context = "\n".join(st.session_state.context)
-    #     question = st.session_state.question
-    #
-    #     messages = [
-    #         {"role": "system",
-    #          "content": f"Answer if the user's question using only the provided document context. If the context contains enough information to answer, give the answer. {tone}"},
-    #         {"role": "user", "content": f"DOCUMENT CONTEXT: \n{context}\n\nQUESTION:\n{question}"}
-    #     ]
-    #     response = client.chat.completions.create(model=MODEL, messages=messages)
-    #     st.write("LLM sample answer: ", response.choices[0].message.content)
 
 with tab3:
     st.title("Mad-Lib maker")
